be7e060c27f0bea83412d8ebd19e6c47c5bcb617
Mohamed Zaher <144987833+MoZaher2@users.noreply.github.com>
2024-07-12 20:39:57 +0300

    delete test folder

--- Test/data/users.js
+++
@@ -1,6 +0,0 @@
-let Users = [
-  { id: 1, usename: "mozaher", email: "mohamedzahertaha@gmail.com" },
-  { id: 2, usename: "khalaf", email: "mokhalaf@gmail.com" },
-];
-
-module.exports = { Users };
--- Test/middlewares/filetype.middleware.js
+++
@@ -1,4 +0,0 @@
-module.exports=(err,req,res,next)=>{
-    if(err)return res.status(400).send({status:"fail",message:err.message})
-    next()
-}
