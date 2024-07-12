20e551e60ba274872715c4217f1300a051e112a5
Mohamed Zaher <144987833+MoZaher2@users.noreply.github.com>
2024-07-12 20:30:42 +0300

    Courses API

---
+++ Test/data/courses.js
@@ -0,0 +1,13 @@
+let Courses = [
+    {
+      id: 1,
+      title: "HTML",
+      price: 150,
+    },
+    {
+      id: 2,
+      title: "CSS",
+      price: 300,
+    },
+  ];
+module.exports={Courses}
---
+++ Test/data/users.js
@@ -0,0 +1,6 @@
+let Users = [
+  { id: 1, usename: "mozaher", email: "mohamedzahertaha@gmail.com" },
+  { id: 2, usename: "khalaf", email: "mokhalaf@gmail.com" },
+];
+
+module.exports = { Users };
